81485ae419914ff23d8ac4a7f4f9e7228f19ca55
Серов Андрей <actsituation@gmail.com>
2022-01-30 22:37:08 +0700

    fix linter

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -2,13 +2,16 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
+import static hexlet.code.Test.hello;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * @author andreiserov
  */
 class AppTest {
 
     @Test void emptyTest() {
-
+        assertEquals("Hello", hello());
     }
 
 }
