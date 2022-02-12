aebc3829306f2894f185bf9d4c8390c1d40ac4ca
Серов Андрей <actsituation@gmail.com>
2022-02-12 17:21:35 +0700

    add layout

--- src/main/java/hexlet/code/Router.java
+++ src/main/java/hexlet/code/Router.java
@@ -10,13 +10,15 @@ public class Router {
 
     private static final Handler INDEX = ctx -> ctx.render("index.html");
     private static final Handler TEST = ctx -> ctx.render("test.html");
+//    private static final Handler CREATE_URL = ctx -> {
+//        ctx.formParam("")
+//    } TODO
 
     public static void register(Javalin app) {
         app
             .get("/hello", ctx -> ctx.result("Hello World"))
             .get("/", INDEX)
-            .get("/test", TEST)
+            .get("/test", TEST);
 //            .post
-            ;
     }
 }
